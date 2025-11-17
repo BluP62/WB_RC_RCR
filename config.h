8d972925e4c9cce19aa2cc412cde17a5d8e37aa2
BluP62 <39796959+BluP62@users.noreply.github.com>
2025-11-17 16:11:36 +0100

    Add files via upload
    Some URL definitions for tibber and and local IPs for the Shelly/Smart-WB

---
+++ config.h
@@ -0,0 +1,18 @@
+
+
+// ----- Webserver -----
+#define WEBSERVER_PORT 80
+
+// ----- URLs -----
+// Please dajust to your IPs and Shelly commands
+#define URL_ON    "http://10.0.0.5/cm?cmnd=Power%20On";
+#define URL_OFF   "http://10.0.0.5/cm?cmnd=Power%20Off";
+#define URL_PARAM "http://10.0.1.0/getParameters";
+
+// Tibber API Endpunkte
+#define TIBBER_LOGIN_URL "https://app.tibber.com/login.credentials";
+#define TIBBER_GQ_URL    "https://app.tibber.com/v4/gql";
+
+// ----- Pins -----
+
+
